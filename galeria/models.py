a684439982053c3b280e9cda0263cf92998eb74b
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-19 13:10:13 -0300

    banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# ORM -- tradução entre o banco de dados e o python puro
+
+class photography(models.Model):
+    # Colunas
+    name = models.CharField(max_length=100, null=False, blank=False)
+    lengend = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    picture = models.CharField(max_length=100,null=False, blank=False)
+
+# Boa prática
+def __str__(self):
+    return f"photography [name={self.name}]"
