cbb5fb696f8b1b3f4db870ed0a2a8c9214204989
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-20 15:18:21 -0300

    Admin crud

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galeria.models import Photography
 
-# Register your models here.
+class ListeningPhotography(admin.ModelAdmin):
+    list_display = ("id", "name", "legend")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Photography, ListeningPhotography)
