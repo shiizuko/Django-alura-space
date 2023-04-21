628e0ce0de50e05eea6bdd01dd5b18d37ed70fcc
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-20 22:34:10 -0300

    personalizando admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,5 +5,7 @@ class ListeningPhotography(admin.ModelAdmin):
     list_display = ("id", "name", "legend")
     list_display_links = ("id", "name")
     search_fields = ("name",)
+    list_filter = ("category",)
+    list_per_page = 10
 
 admin.site.register(Photography, ListeningPhotography)
