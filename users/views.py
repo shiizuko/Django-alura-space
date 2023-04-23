eff24b179b66bf67de55e00c4127b54d192b4760
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-23 18:22:15 -0300

    urls e views - USERS

--- users/views.py
+++ users/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
 
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def register(request):
+    return render(request, 'users/register.html')
+    
