2fbeca33410bb8dd97ea66818e4df1d7eabc5211
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-24 16:27:41 -0300

    estilizando formulário

--- users/forms.py
+++ users/forms.py
@@ -5,12 +5,23 @@ class LoginForms(forms.Form):
         label="Login Name",
         max_length=100,
         required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex:. Maria Antonia",
+            }
+        ),
         )
     password_login = forms.CharField(
         label="Password",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Digite sua senha",
+            },
+        )
     )
    
     
