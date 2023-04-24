07ce9f7d95466e845e4580f0d2c364ddbfe65392
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-24 17:43:40 -0300

    lógica do cadastro 2/2

--- users/views.py
+++ users/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import redirect, render
 from users.forms import LoginForms, RegisterForm
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForms()
     return render(request, 'users/login.html', {'form': form})
 
 def register(request):
+    # verificando a validação do formulário
     form  = RegisterForm()
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        if form["password_1"].value() != form["password_2"].value():
-            return redirect('register')
         
+        
+        if form.is_valid():
+            if form["password_1"].value() != form["password_2"].value():#verificação das senhas (se são iguais) 
+                return redirect('register')
+            #inserimos as infos recebidas no formulário nas variaveis (name, emai, password), para tornar esses dados mais maleaveis na logica
+            name=form["name_register"].value()
+            email=form["email_register"].value()
+            password=form["password_1"].value()
+            
+            #verificando se a informação do username corresponde a algo já existente no banco de dados
+            if User.objects.filter(username=name).exists():
+                return redirect('register')
+
+            #Criando usuário com as informações inseridas no formulário
+            user = User.objects.create_user(
+                username = name,
+                email    = email,
+                password = password
+            )
+            user.save()
+            return redirect('login')
     return render(request, 'users/register.html', {'form': form})
     
