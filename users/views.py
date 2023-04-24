21811427360f3b746162af95d49340f683f6e28c
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-24 17:16:13 -0300

    logica cadastro 1/2

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from users.forms import LoginForms, RegisterForm
 
 def login(request):
@@ -7,5 +7,10 @@ def login(request):
 
 def register(request):
     form  = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form["password_1"].value() != form["password_2"].value():
+            return redirect('register')
+        
     return render(request, 'users/register.html', {'form': form})
     
