3b9ce33d41b912449a3d14a03c49f0fa52142790
Julia Stamborovski <juliashizuko66@gmail.com>
2023-05-12 12:35:25 -0300

    melhorias no projeto

--- setup/settings.py
+++ setup/settings.py
@@ -144,5 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-    messages.SUCCESS: 'success',
+    messages.SUCCESS: 'success'
 }
